3425f3670712d4b42c20f7d5f3ef36b36d00f5d3
SreeCharan <sreecharan309@gmail.com>
2025-02-01 19:25:31 +0530

    Intial commit

---
+++ apps/merchant/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <div className="grid text-red-300 mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        Hi there from merchant app
+      </div>
+    </main>
+  );
+}
